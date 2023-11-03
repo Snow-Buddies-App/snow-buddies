5a28c589588b4dbe682b63e87626d76c7a31a78c
Joseph Slater McArdle <slater.mcardle@outlook.com>
2023-11-02 19:42:44 -0700

    added necessary nuget packages in application project.

--- src/SnowBuddies.Application/AutoMapperConfiguration/ApplicationProfile.cs
+++ src/SnowBuddies.Application/AutoMapperConfiguration/ApplicationProfile.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using SnowBuddies.Application.Dtos;
 using SnowBuddies.Domain.Entities;
 
 namespace SnowBuddies.Application.AutoMapperConfiguration
 {
-    public class ApplicationProfile: Profile
+    public class ApplicationProfile : Profile
     {
         public ApplicationProfile()
         {
