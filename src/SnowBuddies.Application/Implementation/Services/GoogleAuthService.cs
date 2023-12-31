e30bedcc4e523d125a2ab4ff85cd3c754509238a
Victor Ibanichuka <94909597+VIbanichuka@users.noreply.github.com>
2023-12-31 15:46:40 +0000

    Feat: Implement Sign-in and registeration with google

---
+++ src/SnowBuddies.Application/Implementation/Services/GoogleAuthService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SnowBuddies.Application.Dtos;
+using SnowBuddies.Application.Interfaces.IServices;
+
+namespace SnowBuddies.Application.Implementation.Services
+{
+    public class GoogleAuthService : IGoogleAuthService
+    {
+        private const string TokenValidationUrl = "https://www.googleapis.com/oauth2/v2/tokeninfo?access_token={0}";
+        private const string UserInfoUrl = "https://www.googleapis.com/userinfo/v2/me?fields=family_name,email,name,given_name,family_name,picture&access_token={0}";
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public GoogleAuthService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<GoogleTokenValidationData> ValidateAccessTokenAsync(string accessToken)
+        {
+            var formattedUrl = string.Format(TokenValidationUrl, accessToken);
+            var result = await _httpClientFactory.CreateClient().GetAsync(formattedUrl);
+            result.EnsureSuccessStatusCode();
+            var responseAsString = await result.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<GoogleTokenValidationData>(responseAsString)!;
+        }
+
+        public async Task<GoogleUserInfoResponse> GetUserInfoAsync(string accessToken)
+        {
+            var formattedUrl = string.Format(UserInfoUrl, accessToken);
+            var result = await _httpClientFactory.CreateClient().GetAsync(formattedUrl);
+            result.EnsureSuccessStatusCode();
+            var responseAsString = await result.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<GoogleUserInfoResponse>(responseAsString)!;
+        }
+    }
+}
